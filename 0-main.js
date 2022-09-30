9d59c43487d052aa865e7aabb83a99de6e2136be
JC <JC>
2022-09-30 14:10:12 -0500

    advance

--- 0-main.js
+++ 0-main.js
@@ -2,7 +2,9 @@ import redisClient from './utils/redis';
 
 (async () => {
     console.log(redisClient.isAlive());
+
     console.log(await redisClient.get('myKey'));
+    
     await redisClient.set('myKey', 12, 5);
     console.log(await redisClient.get('myKey'));
 
--- utils/redis.js
+++ utils/redis.js
@@ -1,27 +1,26 @@
 import { createClient } from "redis";
 import { promisify } from 'util'
+
 class RedisClient {
 
     constructor() {
         this.client = createClient()
-        this.client.on('connect', () => console.log('entra a esta mierda'))
         this.client.on('error', (err) => console.log('Redis client not connected to the server:', err));
-        
+        this.get_async = promisify(this.client.get).bind(this.client)
+        this.set_async = promisify(this.client.set).bind(this.client)
     }
 
     isAlive() {
-        console.log(this.client.connected)
-        return true
+        return this.client.connected;
     }
 
     async get(key) {
-        const getval = await promisify(this.client.get).bind(this.client);
-        const val = await getval(key);
-        return val;
+        const GET = await this.get_async(key);
+        return GET;
     }
 
     async set(key, val, duration) {
-        await this.client.set(key, val);
+        await this.set_async(key, val);
         await this.client.expire(key, duration);
     }
 
