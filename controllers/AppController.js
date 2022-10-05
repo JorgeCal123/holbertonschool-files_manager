318025731a06cc8e1ed09361ebd458bdbed501ee
JorgeCal123 <ingsistemas68@gmail.com>
2022-10-05 13:42:29 -0500

    all task

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,20 @@
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
+
   static getStatus(req, res) {
     const data = {
-      redis: RedisClient.isAlive(),
-      db: DBClient.isAlive(),
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     };
     return res.status(200).send(data);
   }
 
   static async getStats(req, res) {
     const data = {
-      users: await DBClient.nbUsers(),
-      files: await DBClient.nbFiles(),
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     };
     return res.status(200).send(data);
   }
